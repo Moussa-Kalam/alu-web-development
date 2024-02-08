ff21722c0ac36f44bb885c52c7c373e957fb0379
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-08 09:46:47 +0200

    feat: implement register user method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-""" Authentication Module """
+""" Authentication module """
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
@@ -10,3 +13,20 @@ def _hash_password(password: str) -> str:
         password.encode('utf-8'),
         bcrypt.gensalt()
     ).decode('utf-8')
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Auth.register_user method """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password)
