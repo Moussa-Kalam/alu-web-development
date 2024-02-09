16cdded2a202a4222c48737393a2a3a61d3f82e7
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-09 23:35:12 +0200

    feat: destroy session

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -67,3 +67,8 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroy session """
+        self._db.update_user(user_id, session_id=None)
+        return None
