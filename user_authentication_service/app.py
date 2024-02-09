909c29ed50215f598da8ba15185abafa1a7e55b4
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-09 23:31:22 +0200

    feat: implement login

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Basic flask app"""
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 AUTH = Auth()
@@ -23,3 +23,20 @@ def register_user():
     password = request.form.get("password")
     user = AUTH.register_user(email, password)
     return jsonify({"email": user.email, "message": "user created"})
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """ Log in """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        if session_id:
+            response = make_response(
+                jsonify({"email": email,
+                         "session_id": session_id,
+                         "message": "logged in"}))
+            response.set_cookie('session_id', session_id)
+            return response
+    abort(401)
