3d4c6e2789689ef3125e5008b27635fa06130d67
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-09 23:45:37 +0200

    feat: implement update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -82,3 +82,14 @@ class Auth:
             return token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Update password """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password)
+            self._db.update_user(user.id, reset_token=None)
+        except NoResultFound:
+            raise ValueError
+        return None
