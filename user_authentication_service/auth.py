edab894dd0512355f5a4514d514a966a6c8e3a2b
Moussa-Kalam <m.amzat@alustudent.com>
2024-02-09 23:33:29 +0200

    feat: find user by session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -57,3 +57,13 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """ Find user by session ID """
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
